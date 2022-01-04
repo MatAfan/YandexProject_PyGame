0323b9b3e9c43b782b96b12347a355b124646c84
Афанасьев <matvey.afanasev986@gmail.com>
2022-01-04 15:48:14 +0300

    Обновление load_image

--- load_image().py
+++ load_image().py
@@ -1,13 +1,11 @@
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    # если файл не существует, то выходим
+def load_image(name, color_key=None):
+    fullname = os.path.join('../data', name)
     if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
+        print(f'Такого файла нет')
         sys.exit()
     image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
     return image
